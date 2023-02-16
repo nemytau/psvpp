de16ce51c18919e44a61202ba984ecb0e329fa70
nemytau <nemytov.t@gmail.com>
2023-02-16 20:06:25 +0100

    basic vessel class

--- alns/vessel.py
+++ alns/vessel.py
@@ -1,12 +1,37 @@
 class Vessel:
-    def __init__(self, name, deck_capacity, bulk_capacity, speed, v_type):
+    def __init__(self,
+                 name: str,
+                 deck_capacity: int,
+                 bulk_capacity: int,
+                 speed: float,
+                 vessel_type: str,
+                 fuel_consumption: float,
+                 cost: int) -> None:
         self.name = name
         self.deck_capacity = deck_capacity
         self.bulk_capacity = bulk_capacity
         self.speed = speed
-        self.v_type = v_type
+        self.vessel_type = vessel_type
         self.deck_cargo = 0
         self.bulk_cargo = 0
+        self.fuel_consumption = fuel_consumption
+        self.cost = cost
+
+    @classmethod
+    def default_charter_vessel(cls, n):
+        return cls(
+            name=f'v{n}',
+            deck_capacity=100,
+            bulk_capacity=1000,
+            speed=12,
+            vessel_type='ordinal',
+            fuel_consumption=0.43,
+            cost=0
+        )
+
+    def __repr__(self):
+        return f'Vessel {self.name} ' \
+               f'deck: {self.deck_cargo}/{self.deck_capacity}'
 
     def add_deck_cargo(self, amount):
         if self.deck_cargo + amount > self.deck_capacity:
@@ -19,3 +44,15 @@ class Vessel:
             return False
         else:
             self.bulk_cargo += amount
+
+    def remove_deck_cargo(self, amount):
+        if self.deck_cargo - amount > 0:
+            return False
+        else:
+            self.deck_cargo -= amount
+
+    def remove_bulk_cargo(self, amount):
+        if self.bulk_cargo - amount > 0:
+            return False
+        else:
+            self.bulk_cargo -= amount
