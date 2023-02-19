30fa517b9c39a93ac5317c3445216e49c5f714ce
nemytau <nemytov.t@gmail.com>
2023-02-19 21:49:34 +0100

    init io

---
+++ alns/io.py
@@ -0,0 +1,34 @@
+import pandas as pd
+
+
+def read_installation_dataset(dataset_name):
+    pass
+
+
+def write_installation_dataset(dataset_name):
+    pass
+
+
+def read_fleet_dataset(dataset_name):
+    pass
+
+
+def write_fleet_dataset(dataset_name):
+    pass
+
+
+def read_solution(solution_name):
+    pass
+
+
+def write_solution(solution_name):
+    pass
+
+
+def read_installation_raw_data(filename):
+    pass
+
+
+def read_fleet_raw_data(filename):
+    pass
+
