b067da39ddb0d12214509c6d0145c069c125d699
nemytau <nemytov.t@gmail.com>
2023-02-19 21:54:12 +0100

    init data generation script

--- data_generation.py
+++ data_generation.py
@@ -1,10 +1,12 @@
 from alns.data_generator import *
+from alns.coord import Coord
 
 
 def main():
-    base, insts = generate_installation_dataset()
+    insts = generate_installation_dataset('small_1')
     fleet = generate_fleet_dataset()
+    base = generate_base('FMO')
 
-
+import numpy as np
 if __name__ == '__main__':
     main()
