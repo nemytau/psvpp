fixed wrond distance calculation, removed offshore point from coords

alns/coord.py
@@ -7,13 +7,15 @@ class Coord:
         """
         Create Coord instance.
 
-        :param x: longitude.
+        :param x: latitude.
         :type x: float
-        :param y: latitude.
+        :param y: longitude.
         :type y: float
         """
         self.x = x
         self.y = y
+        self.lat = x
+        self.lon = y
         self.coord = (x, y)
         self.rad_coord = tuple(np.radians(self.coord))
 
@@ -34,9 +36,9 @@ class Coord:
         """
         Calculate geodesic distance between two points.
 
-        :param coord1: tuple of coordinates of start point in radians.
+        :param coord1: tuple of coordinates (lat, lon) of start point in decimal degrees.
         :type coord1: tuple[float, float]
-        :param coord2: tuple of coordinates of end point in radians.
+        :param coord2: tuple of coordinates (lat, lon) of end point in decimal degrees.
         :type coord2: tuple[float, float]
         :param unit: unit name from ['nmi', 'km'], defaults to 'nmi'.
         :type unit: str
@@ -62,5 +64,8 @@ class Coord:
         :return: distance in selected units.
         :rtype: float
         """
-        distance = self.geo_distance(self.rad_coord, other.rad_coord, unit)
+        distance = self.geo_distance(self.coord, other.coord, unit)
         return distance
+
+    def __repr__(self):
+        return f'[{self.x}, {self.y}]'